18fa721b25b8c64b78654b9c481155d3cbe4a7c0
fnakhsan <fn.akhsan@gmail.com>
2023-01-10 18:57:00 +0700

    Add dependencies and splash screen

--- app/src/main/java/com/example/githubuser3/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/githubuser3/ui/main/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.example.githubuser3
+package com.example.githubuser3.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githubuser3.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_GithubUser3)
         setContentView(R.layout.activity_main)
     }
 }
