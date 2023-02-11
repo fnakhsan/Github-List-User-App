8d2b6a705fa6987825db29447daac87ed02008e9
fnakhsan <fn.akhsan@gmail.com>
2023-02-11 17:06:32 +0700

    Add bottom navigation, add some bug too :v (does not have a NavController set) at this scenario (home -> another fragment -> home -> detail)

--- app/src/main/java/com/example/githubuser3/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubuser3/ui/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.githubuser3.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.githubuser3.R
 import com.example.githubuser3.databinding.ActivityMainBinding
+import com.example.githubuser3.ui.favorite.FavoriteFragment
+import com.example.githubuser3.ui.home.HomeFragment
+import com.example.githubuser3.ui.setting.SettingFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,5 +17,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.bottom_nav_home -> setCurrentFragment(HomeFragment())
+                R.id.bottom_nav_favorite -> setCurrentFragment(FavoriteFragment())
+                R.id.bottom_nav_setting -> setCurrentFragment(SettingFragment())
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.nav_host, fragment)
+        commit()
     }
 }
