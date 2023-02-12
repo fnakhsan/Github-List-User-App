4ac83aee57bc60692debda03ef34653ae82dc6d7
fnakhsan <fn.akhsan@gmail.com>
2023-02-12 12:04:36 +0700

    okeh, bug solved, i think u must put navController first before onCreate (with late init ofc) and then create same id for every fragment navigation by matching it to the id of each menu.

--- app/src/main/java/com/example/githubuser3/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubuser3/ui/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.githubuser3.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.githubuser3.R
 import com.example.githubuser3.databinding.ActivityMainBinding
-import com.example.githubuser3.ui.favorite.FavoriteFragment
-import com.example.githubuser3.ui.home.HomeFragment
-import com.example.githubuser3.ui.setting.SettingFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_GithubUser3)
@@ -18,18 +18,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        binding.bottomNav.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.bottom_nav_home -> setCurrentFragment(HomeFragment())
-                R.id.bottom_nav_favorite -> setCurrentFragment(FavoriteFragment())
-                R.id.bottom_nav_setting -> setCurrentFragment(SettingFragment())
-            }
-            true
-        }
-    }
-
-    private fun setCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
-        replace(R.id.nav_host, fragment)
-        commit()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNav, navController)
     }
 }
